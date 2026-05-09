b45a6b95c1a643fca85bc61696281e754e542bc7
Jake Deery <jake@deeryme.net>
2026-05-09 22:25:27 +0100

    fix: swop to libravatar, its faster

--- _plugins/gravitar.rb
+++ _plugins/gravitar.rb
@@ -2,7 +2,7 @@ require 'digest'
 
 module Jekyll
     module GravitarURL
-        def get_gravitar_url(input, size=200)
+        def get_gravatar_url(input, size=200)
             "https://gravatar.com/avatar/" + (Digest::SHA256.hexdigest input) + "?s=" + size.to_s
         end
     end
---
+++ _plugins/libravatar.rb
@@ -0,0 +1,11 @@
+require 'digest'
+
+module Jekyll
+    module LibravatarURL
+        def get_libravatar_url(input, size=200)
+            "https://seccdn.libravatar.org/avatar/" + (Digest::SHA256.hexdigest input) + "?s=" + size.to_s
+        end
+    end
+end
+  
+Liquid::Template.register_filter(Jekyll::LibravatarURL)
